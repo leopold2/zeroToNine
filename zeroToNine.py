64ab61351c4fdce433c2ff3134fc8189b8d10e29
Leopold2vonWien <leopold2vonwien@localhost.localdomain>
2018-12-26 22:48:45 +0300

    hey

--- zeroToNine.py
+++ zeroToNine.py
@@ -5,7 +5,7 @@ root=Tk() #Initiating the frame
 w, h = root.winfo_screenwidth(), root.winfo_screenheight()
 root.overrideredirect(1)
 root.geometry("%dx%d+0+0" % (w, h))
-col=153
+col=153 #you know what this is
 row=60
 num=[random.randint(0,9) for i in range(col*row)] #Creating the first set of numbers to be displayed
 st=""
